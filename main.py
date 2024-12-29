caa0baaab1ae7348a6f2ab10bb14de920a010303
megops <megan.fcoyle@gmail.com>
2024-12-28 19:06:51 -0500

    added game launch and exit

--- main.py
+++ main.py
@@ -2,9 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_state = True
+    while (game_state == True):
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
 
 
 if __name__ == "__main__":
