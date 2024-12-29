c71c5e2f8a1a422067266340115e00adb7261ad2
megops <megan.fcoyle@gmail.com>
2024-12-28 19:55:23 -0500

    added 60fps clock

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     game_state = True
+
     while (game_state == True):
         screen.fill("black")
-        pygame.display.flip()
+        pygame.display.flip()   
+        dt = clock.tick(60)/1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
